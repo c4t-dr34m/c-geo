e8cab881fc1e3068fe1f635fe5efa991ad3a720f
carnero <carnero@carnero.cc>
2011-01-16 09:53:58 +0100

    Fix of fix

--- src/carnero/cgeo/cgCacheListAdapter.java
+++ src/carnero/cgeo/cgCacheListAdapter.java
@@ -399,12 +399,12 @@ public class cgCacheListAdapter extends ArrayAdapter<cgCache> {
 		}
 
 		if (cache.vote != null && cache.vote > 0) {
-			if (cache.vote > 0) {
-				holder.favourite.setBackgroundResource(ratingBcgs[0]);
+			if (cache.vote > 3) {
+				holder.favourite.setBackgroundResource(ratingBcgs[2]);
 			} else if (cache.vote > 1) {
 				holder.favourite.setBackgroundResource(ratingBcgs[1]);
-			} else if (cache.vote > 3) {
-				holder.favourite.setBackgroundResource(ratingBcgs[2]);
+			} else if (cache.vote > 0) {
+				holder.favourite.setBackgroundResource(ratingBcgs[0]);
 			} else {
 				if (settings.skin == 1) {
 					holder.favourite.setBackgroundResource(R.drawable.favourite_background_light);
@@ -413,12 +413,12 @@ public class cgCacheListAdapter extends ArrayAdapter<cgCache> {
 				}
 			}
 		} else if (cache.rating != null && cache.rating > 0) {
-			if (cache.rating > 0.0) {
-				holder.favourite.setBackgroundResource(ratingBcgs[0]);
+			if (cache.rating >= 3.4) {
+				holder.favourite.setBackgroundResource(ratingBcgs[2]);
 			} else if (cache.rating >= 1.5) {
 				holder.favourite.setBackgroundResource(ratingBcgs[1]);
-			} else if (cache.rating >= 3.5) {
-				holder.favourite.setBackgroundResource(ratingBcgs[2]);
+			} else if (cache.rating > 0.0) {
+				holder.favourite.setBackgroundResource(ratingBcgs[0]);
 			} else {
 				if (settings.skin == 1) {
 					holder.favourite.setBackgroundResource(R.drawable.favourite_background_light);
