31497e15bc5354ceff7dd8b7fc870ec99759a3a5
carnero <carnero@carnero.cc>
2011-03-16 22:27:46 +0100

    Just make it secure

--- src/carnero/cgeo/cgeotrackables.java
+++ src/carnero/cgeo/cgeotrackables.java
@@ -57,7 +57,7 @@ public class cgeotrackables extends Activity {
 						Button oneTb = (Button) oneTbPre.findViewById(R.id.button);
 
 						if (trackable.name != null) {
-							oneTb.setText(Html.fromHtml(trackable.name), Button.BufferType.SPANNABLE);
+							oneTb.setText(Html.fromHtml(trackable.name).toString());
 						} else {
 							oneTb.setText("some trackable");
 						}
